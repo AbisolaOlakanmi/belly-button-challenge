5c291a99421e071b99ca4d051b882bb1fa6b2aea
AbisolaOlakanmi <123206066+AbisolaOlakanmi@users.noreply.github.com>
2023-06-01 15:50:11 -0400

    Update app.js

--- Starter_Code/StarterCode/static/js/app.js
+++ Starter_Code/StarterCode/static/js/app.js
@@ -4,3 +4,22 @@ const url ="https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v
 d3.json(url).then(function(data){
     console.log(data);
 })
+
+let dropdown;
+let optSample;
+
+//var OTUdata = d3.json(url).then((data) => console.log(data.samples[0]));
+d3.json(url).then((sample) => {
+    //dropdown list values
+    dropdown = sample.names;
+    //display test subject id dropdown
+    displayOptions(dropdown);
+    
+    //display default  demographic and charts (using first sample, index 0)
+    displayData(sample, 0, -1);
+
+    optSample = sample;
+    // On change to the DOM, call optionChanged()
+    d3.select("#selDataset").on("change", optionChanged(this.value));
+
+});
