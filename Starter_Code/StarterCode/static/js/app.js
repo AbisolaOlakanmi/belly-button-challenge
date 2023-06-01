2e547cc40ba604d764c1aaab1d6d0ec121c0a874
AbisolaOlakanmi <123206066+AbisolaOlakanmi@users.noreply.github.com>
2023-06-01 18:25:47 -0400

    Update app.js

--- Starter_Code/StarterCode/static/js/app.js
+++ Starter_Code/StarterCode/static/js/app.js
@@ -1,25 +1,90 @@
 const url ="https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// using d3
-d3.json(url).then(function(data){
-    console.log(data);
-})
+// // using d3
+// d3.json(url).then(function(data){
+//     console.log(data);
+// })
 
-let dropdown;
-let optSample;
+/// Demographic Info
+function panelInfo(id) {
+    d3.json(url).then(function (data) {
+        let sampleData = data;
+        let metadata = sampleData.metadata;
+        let identifier = metadata.filter(sample =>
+            sample.id.toString() === id)[0];
+        let panel = d3.select('#sample-metadata');
+        panel.html('');
+        Object.entries(identifier).forEach(([key, value]) => {
+            panel.append('h6').text(`${key}: ${value}`);
+        })
+    })
+};
 
-//var OTUdata = d3.json(url).then((data) => console.log(data.samples[0]));
-d3.json(url).then((sample) => {
-    //dropdown list values
-    dropdown = sample.names;
-    //display test subject id dropdown
-    displayOptions(dropdown);
-    
-    //display default  demographic and charts (using first sample, index 0)
-    displayData(sample, 0, -1);
+//Plots
+function Plots(id) {
+    d3.json(url).then(function (data) {
+        let sampleData = data;
+        let samples = sampleData.samples;
+        let identifier = samples.filter(sample => sample.id === id);
+        let filtered = identifier[0];
+        let OTUvalues = filtered.sample_values.slice(0, 10).reverse();
+        let OTUids = filtered.otu_ids.slice(0, 10).reverse();
+        let labels = filtered.otu_labels.slice(0, 10).reverse();
+        let barTrace = {
+            x: OTUvalues,
+            y: OTUids.map(object => 'OTU ' + object),
+            name: labels,
+            type: 'bar',
+            orientation: 'h'
+        };
+        let barLayout = {
+            title: `Top 10 OTUs for Subject ${id}`,
+            xaxis: { title: 'Sample Values' },
+            yaxis: { title: 'OTU ID' }
+        };
+        let barData = [barTrace];
+        Plotly.newPlot('bar', barData, barLayout);
+        let bubbleTrace = {
+            x: filtered.otu_ids,
+            y: filtered.sample_values,
+            mode: 'markers',
+            marker: {
+                size: filtered.sample_values,
+                color: filtered.otu_ids,
+                colorscale: 'Portland'
+            },
+            text: filtered.otu_labels,
+        };
+        let bubbleData = [bubbleTrace];
+        let bubbleLayout = {
+            title: `OTUs for Subject ${id}`,
+            xaxis: { title: 'OTU ID' },
+            yaxis: { title: 'Sample Values' }
+        };
+        Plotly.newPlot('bubble', bubbleData, bubbleLayout);
+    })
+};
 
-    optSample = sample;
-    // On change to the DOM, call optionChanged()
-    d3.select("#selDataset").on("change", optionChanged(this.value));
+//Build new upon ID change
+function optionChanged(id) {
+    Plots(id);
+    panelInfo(id);
+};
 
-});
+//Test Subject Dropdown and initial function
+function init() {
+    let dropDown = d3.select('#selDataset');
+    let id = dropDown.property('value');
+    d3.json(url).then(function (data) {
+        sampleData = data;
+        let names = sampleData.names;
+        let samples = sampleData.samples;
+        Object.values(names).forEach(value => {
+            dropDown.append('option').text(value);
+        })
+        panelInfo(names[0]);
+        Plots(names[0])
+    })
+};
+
+init();
