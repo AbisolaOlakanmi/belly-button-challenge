3987206dff49cb93fad8457b50aa5724bde08836
AbisolaOlakanmi <123206066+AbisolaOlakanmi@users.noreply.github.com>
2023-06-01 22:29:09 -0400

    new update

--- Starter_Code/StarterCode/static/js/app.js
+++ Starter_Code/StarterCode/static/js/app.js
@@ -1,11 +1,17 @@
-const url ="https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+url ="https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 // // using d3
 // d3.json(url).then(function(data){
 //     console.log(data);
 // })
 
-/// Demographic Info
+// let sampleNames = data.names;
+//     let sampleMetadata = data.metadata; 
+//     let samples = data.samples;
+//     let sampleNumber = 940
+//     let resultsArray = samples.filter(sampleObj => sampleObj.id == sampleNumber)
+
+// creating the functionns 
 function panelInfo(id) {
     d3.json(url).then(function (data) {
         let sampleData = data;
@@ -20,7 +26,7 @@ function panelInfo(id) {
     })
 };
 
-//Plots
+//Building the bar chart
 function Plots(id) {
     d3.json(url).then(function (data) {
         let sampleData = data;
@@ -44,6 +50,8 @@ function Plots(id) {
         };
         let barData = [barTrace];
         Plotly.newPlot('bar', barData, barLayout);
+
+//Building the bubble chart
         let bubbleTrace = {
             x: filtered.otu_ids,
             y: filtered.sample_values,
@@ -57,9 +65,6 @@ function Plots(id) {
         };
 
 
-//Building the bubbleData
-
-
         let bubbleData = [bubbleTrace];
         let bubbleLayout = {
             title: `OTUs for Subject ${id}`,
@@ -70,13 +75,12 @@ function Plots(id) {
     })
 };
 
-//Build new upon ID change
 function optionChanged(id) {
     Plots(id);
     panelInfo(id);
 };
 
-//Test Subject Dropdown and initial function
+// Dropdown function and initial function
 function init() {
     let dropDown = d3.select('#selDataset');
     let id = dropDown.property('value');
@@ -92,11 +96,99 @@ function init() {
     })
 };
 
-init();
+
+
+// The Bonus part
 
 
 
-
-
-
-
+function buildGauge(wfreq) {
+    // Enter the washing frequency between 0 and 180
+    let level = parseFloat(wfreq) * 20;
+  
+    // Trig to calc meter point
+    let degrees = 180 - level;
+    let radius = 0.5;
+    let radians = (degrees * Math.PI) / 180;
+    let x = radius * Math.cos(radians);
+    let y = radius * Math.sin(radians);
+  
+    // Path: may have to change to create a better triangle
+    let mainPath = "M -.0 -0.05 L .0 0.05 L ";
+    let pathX = String(x);
+    let space = " ";
+    let pathY = String(y);
+    let pathEnd = " Z";
+    let path = mainPath.concat(pathX, space, pathY, pathEnd);
+  
+    let data = [
+      {
+        type: "scatter",
+        x: [0],
+        y: [0],
+        marker: { size: 12, color: "850000" },
+        showlegend: false,
+        name: "Freq",
+        text: level,
+        hoverinfo: "text+name"
+      },
+      {
+        values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+        rotation: 90,
+        text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+        textinfo: "text",
+        textposition: "inside",
+        marker: {
+          colors: [
+            "rgba(0, 105, 11, .5)",
+            "rgba(10, 120, 22, .5)",
+            "rgba(14, 127, 0, .5)",
+            "rgba(110, 154, 22, .5)",
+            "rgba(170, 202, 42, .5)",
+            "rgba(202, 209, 95, .5)",
+            "rgba(210, 206, 145, .5)",
+            "rgba(232, 226, 202, .5)",
+            "rgba(240, 230, 215, .5)",
+            "rgba(255, 255, 255, 0)"
+          ]
+        },
+        labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+        hoverinfo: "label",
+        hole: 0.5,
+        type: "pie",
+        showlegend: false
+      }
+    ];
+  
+    let layout = {
+      shapes: [
+        {
+          type: "path",
+          path: path,
+          fillcolor: "850000",
+          line: {
+            color: "850000"
+          }
+        }
+      ],
+      title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
+      height: 500,
+      width: 500,
+      xaxis: {
+        zeroline: false,
+        showticklabels: false,
+        showgrid: false,
+        range: [-1, 1]
+      },
+      yaxis: {
+        zeroline: false,
+        showticklabels: false,
+        showgrid: false,
+        range: [-1, 1]
+      }
+    };
+  
+    Plotly.newPlot("gauge", data, layout);
+  }
+  
+  init();
